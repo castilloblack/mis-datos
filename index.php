4580f41432624a550149f832370c08b9eeba8966
Castillo Álvarado Álvarado <125686686+castilloblack@users.noreply.github.com>
2023-03-29 13:56:51 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>mis datos</title>
+  </head>
+  <body bgcolor="greenblueyellow">
+
+<?php
+echo 'nombre='.'castillo alvarado mario alejandro';
+?>
+<br>
+<?php
+echo 'calle='.'encino';
+?>
+<br>
+<?php
+echo 'numero de casa='.'8441234567';
+?>
+<br>
+<?php
+echo 'colonia='.'el nogalar';
+?>
+<br>
+<?php
+echo 'municipio='.'saltillo coahuila';
+?>
+<br>
+<?php
+echo 'telefono='.'8442653118';
+?>
+  </body>
+</html>
